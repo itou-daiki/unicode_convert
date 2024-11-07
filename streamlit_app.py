cda6ffffae18c5c4fc8082bf55ef4f93d44314c8
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2024-11-07 13:20:04 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,15 @@
-# Streamlitライブラリをインポート
 import streamlit as st
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+# Webアプリのタイトル
+st.title("日本語文字の文字コード表示アプリ")
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+# 入力フォーム
+input_text = st.text_input("日本語の文字を入力してください（1文字のみ）:")
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
-
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
+# 入力がある場合に文字コードを表示
+if input_text:
+    if len(input_text) == 1:
+        char_code = ord(input_text)
+        st.write(f"文字 '{input_text}' の文字コードは: {char_code}")
     else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
-
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
-
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
-
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+        st.error("1文字のみ入力してください。")
